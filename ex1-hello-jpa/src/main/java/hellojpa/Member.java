e21d1a700d95ddbaedf7468244b11b6ea9691843
ImJaePang <narushel@naver.com>
2026-05-19 07:17:38 +0900

    양방향 일부수정

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -43,8 +43,10 @@ public class Member {
         return team;
     }
 
-    public void setTeam(Team team) {
+    public void changeTeam(Team team) {
         this.team = team;
+
+        team.getMembers().add(this);
     }
 //    public Long getTeamId() {
 //        return teamId;
--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,11 +14,22 @@ public class Team {
 
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+
     public Team() {
     }
 
     public Long getId() {
         return id;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 
     public void setId(Long id) {
