753d8c52e66d4547b7a0f5d5d95543cd1585b929
Jaeyoung Im <narushel09@gmail.com>
2026-06-04 12:28:41 +0900

    실전 예제 6 - 값 타입 매핑

--- ex4-jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ ex4-jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -8,12 +8,41 @@ public class Delivery {
     @Id @GeneratedValue
     private Long Id;
 
-    private  String city;
-    private  String street;
-    private String zipcode;
+    private Address address;
     private DeliveryStatus deliveryStatus;
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
--- ex4-jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ ex4-jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -10,9 +10,9 @@ public class Member {
     private Long id;
 
     private String name;
-    private  String city;
-    private  String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     public Member() {
     }
@@ -33,28 +33,11 @@ public class Member {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
 }
